75f82ff59b373ab2b256eb8375f145316fae39eb
AlexAlbala <alexalbala88@gmail.com>
2015-06-04 14:02:13 +0200

    ** fixed configurations

--- index.js
+++ index.js
@@ -2,5 +2,8 @@ module.exports.config = require('./plugin.json');
 
 module.exports.init = function(injector, oauthConfig){
 	oauthConfig.storage = require('./lib/storage.js');	
+	oauthConfig.authorize_uri = '/oauth/authorize';
+	oauthConfig.login_uri = '/oauth/login';
+	oauthConfig.access_token_uri = '/oauth/token';
 	injector.security = require('./lib')(injector, oauthConfig);
 }
